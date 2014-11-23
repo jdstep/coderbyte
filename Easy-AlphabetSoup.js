e9ba97a83a39e13220ab7f9930e4433e8a16769e
jonathanenclave <jonathandavis5@icloud.com>
2014-11-23 19:02:46 +0900

    Update Easy-AlphabetSoup.js

--- Easy-AlphabetSoup.js
+++ Easy-AlphabetSoup.js
@@ -1,7 +1,7 @@
 // takes an unsorted array of only letters
 // returns the array sorted alphabetically
 function AlphabetSoup(str) { 
-	var tmpStr;
+  var tmpStr;
   tmpStr = str.split('');
   tmpStr = tmpStr.sort();
   tmpStr = tmpStr.join('');
