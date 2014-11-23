0d39b91fa99fac6c8639305c419ccaa8a924502b
jonathanenclave <jonathandavis5@icloud.com>
2014-11-23 18:56:00 +0900

    Updated Easy-CheckNums.js

--- Easy-CheckNums.js
+++ Easy-CheckNums.js
@@ -3,7 +3,6 @@
 // returns false if the 1st argument is greater than the 2nd argument
 // returns the string "-1" if the two arguments are equal
 function CheckNums(num1,num2) { 
-
   if (num1>num2){
    return false;
   }
@@ -12,8 +11,5 @@ function CheckNums(num1,num2) {
   }
   else if (num1 == num2){
    return "-1";
-  }
-         
+  }      
 }
-
-console.log(CheckNums(4,4));
